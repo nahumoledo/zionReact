bd0581a67e511c5c114a5df36b40b82e0c88fd2d
nahumoledo <91441415+nahumoledo@users.noreply.github.com>
2022-09-27 23:50:29 -0300

    Add files via upload

---
+++ src/components/ItemDetailContainer.js
@@ -0,0 +1,31 @@
+import React from "react";
+import data from "./mockData";
+import { useEffect, useState } from "react";
+import ItemDetail from "./ItemDetail";
+import {useParams} from 'react-router-dom'
+
+const getProducts = new Promise((resolve, reject) => {
+    setTimeout(()=> {
+        resolve(data)
+    }, 2000)
+})
+
+function ItemDetailContainer() {
+    const [producto, setProducto] = useState([]);
+    const {id} = useParams()
+
+    useEffect(() => {
+        getProducts.then((res)=>{ 
+            const myProd = res.find(p => p.id === id) 
+                setProducto(myProd) 
+        })
+    }, [])
+
+    return(
+        <>
+            <ItemDetail data={producto}/>
+        </>
+    )
+}
+
+export default ItemDetailContainer
---
+++ src/components/itemList/ItemList.js
@@ -0,0 +1,27 @@
+import Item from "../Item"
+import ItemListContainer from "../itemListContainer/ItemListContainer"
+import data from "../mockData"
+import React from "react"
+import { Link } from "react-router-dom"
+
+
+const ItemList = ({lista}) => {
+
+    return(
+        <div>
+            {lista.map((data) => (
+                <div key={data.id} id="prod">
+                   <Link to={'/item/' + data.id}> 
+                   <Item
+                     nombre={data.nombre}
+                     precio={data.precio}
+                     img={data.img}
+                 />
+                   </Link>
+                </div>
+            ))}
+        </div>   
+     )
+}
+
+export default ItemList
