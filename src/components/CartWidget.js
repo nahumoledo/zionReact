1978cdf8872b682df607f8a0304d142efdd80ec3
nahumoledo <91441415+nahumoledo@users.noreply.github.com>
2022-10-10 00:40:51 -0300

    Add files via upload

---
+++ src/components/CartWidget.js
@@ -0,0 +1,24 @@
+import React from "react"
+import shopping_cart from '../Img/shopping_cart.png'
+import { Link } from "react-router-dom"
+import { useContext } from "react"
+import Cart from "./cart/cart"
+
+function CartWidget() {
+    return(
+            <ul> <Link to={'/cart'}></Link>
+                <li class="submenu">
+                    <button id="open" href="#" class='btn btn-success pull-right'><img src={shopping_cart} id="carrito" alt=""/><i class="fa-solid fa-cart-shopping"></i><span id="counter" class="cartcontents"></span></button>
+                    <div id="modal_container" class="modal-container">
+                        <div id="modalCart" class="modal-cart">
+                            <div id="cart-container">
+                                <Cart/>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+    )
+}
+
+export default CartWidget
---
+++ src/components/ItemDetail.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ItemCount from "./ItemCount";
+import { useContext, useState } from "react";
+import { CartContext } from "../context/cartContext";
+
+function ItemDetail({ data }) {
+    const {addToCart} = useContext(CartContext)
+    const [cantProductos, setCantProductos] = useState(0) 
+    
+    function onAdd(data, cantProductos) {
+        addToCart(data.item, cantProductos)
+    }
+    return(
+        <div className="detail-container">
+            <img width={'500px'} src={`./proyecto-app/src/Img/${data.img}`} alt={data.nombre} />
+            <h2>{data.nombre}</h2>
+            <h3>{data.precio}</h3>
+            <ItemCount cantProductos={cantProductos} setCantProductos={setCantProductos}/>
+            <div>
+                <button className="add-btn buy" onClick={() => onAdd(data, setCantProductos)}>Agregar al Carrito</button>
+            </div>
+        </div>   
+     )
+}
+
+export default ItemDetail
