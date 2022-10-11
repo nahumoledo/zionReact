901545c920a76fa225edb9514bf61c959631c8c8
nahumoledo <91441415+nahumoledo@users.noreply.github.com>
2022-10-11 20:16:47 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import React from 'react';
+import NavBar from './components/Navbar/NavBar'
+import ItemListContainer from './components/itemListContainer/ItemListContainer';
+import ItemDetailContainer from './components/ItemDetailContainer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { CartProvider } from './context/cartContext';
+import Cart from './components/cart/cart';
+
+
+
+
+function App() {
+  return (
+    <CartProvider>
+      <BrowserRouter>
+      <NavBar />
+        <Routes>
+          <Route exact path='/' element={<ItemListContainer />} />
+          <Route path='cart' element={<Cart />}/>
+          <Route exact path='/item/:id' element={<ItemDetailContainer />} />
+        </Routes>
+      </BrowserRouter>
+    </CartProvider>
+  );
+}
+export default App;
