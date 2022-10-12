8b9c9dc807167e32c37a78bd122a4da633b261fc
nahumoledo <91441415+nahumoledo@users.noreply.github.com>
2022-10-11 22:32:48 -0300

    Update Item.js

--- src/components/Item.js
+++ src/components/Item.js
@@ -8,7 +8,7 @@ const Item = ({imageId, price, title, id}) => {
   
     return(
         <div>
-            <img width={'200px'} src={`/Img` + imageId} alt={title} />
+            <img width={'200px'} src={`./Img/` + imageId} alt={title} />
             <h2>{title}</h2>
             <h3>{price}</h3>
             <Link to={`/item/${id}`}>
